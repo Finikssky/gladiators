3f8016f44ce3fe39c23c52fdfa90572ffad20393
vasily.kuznetsov <vasily.kuznetsov@eltex.loc>
2016-04-27 15:54:58 +0600

    [TEST] Add test script to test functionality

---
+++ src/test.py
@@ -0,0 +1,25 @@
+import weapons
+import skills
+import gladiators
+
+me = gladiators.Gladiator("Sneek")
+me.set_primary_attr("agility", 4)
+me.set_primary_attr("strength", 2)
+me.set_primary_attr("endurance", 2)
+me.set_primary_attr("luck", 2)
+
+
+en = gladiators.Gladiator("Elefant")
+en.ai = 1
+en.set_primary_attr("agility", 2)
+en.set_primary_attr("strength", 3)
+en.set_primary_attr("endurance", 4)
+en.set_primary_attr("luck", 1)
+
+flamberg = weapons.Weapon("flamberg", weapons.WeaponHandType.TwoHand, weapons.WeaponBaseType.Sword, 15, 7.0)
+knife = weapons.Weapon("knife", weapons.WeaponHandType.OneHand, weapons.WeaponBaseType.Dagger, 3, 0.8)
+
+en.take_weapon(flamberg, "right")
+
+me.take_weapon(knife, "right")
+me.take_weapon(knife, "left")
