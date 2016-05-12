6d3e089023291ce47bfdf562b150a4bf4c8fbf64
finikssky <none>
2016-05-12 16:19:44 +0600

    [ARENA] add function to generate arena

--- src/arena.py
+++ src/arena.py
@@ -1,4 +1,5 @@
 import jsonpickle
+import random
 
 class ArenaType():
 	Circle = 0
@@ -11,8 +12,14 @@ class ArenaCoverType():
 	Stone = 3
 
 class Arena():
-	def __init__(self, name, size, stype, cover):
+	def __init__(self, name, size = 1, stype = ArenaType.Circle, cover = ArenaCoverType.Sand):
 		self.name = name
 		self.size = size
 		self.type = stype
 		self.cover = cover
+
+	def generate(self, name = "unnamed"):
+		self.name = name
+		self.type = random.randint(0, 1)
+		self.cover = random.randint(0, 3)
+		self.size = random.randint(100, 500)
